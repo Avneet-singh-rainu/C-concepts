0a3e7d37c3efdc94f1efdfb563c6dc5ff48cae94
Avneet Singh <avneetsingh.8979@gmail.com>
2025-04-21 18:56:27 +0530

    object mapping using class and extension method

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,33 +1,63 @@
-﻿public interface IMessageService {
-    void Send ( string message );
+﻿using ConsoleApp2;
+
+class Program
+{
+    static void Main()
+    {
+
+        User user = new User();
+        user.name = "John";
+        user.age = 25;
+        user.course = "C# Programming";
+
+        Student student = new Student();
+        // mapping using the Mapper class
+        //student = Mapper.MapToStudent( user );
+
+        // mapping using the extension method
+        student = user.ExtensionMapToStudent();
+
+
+
+        Console.WriteLine($"Name: {student.name}, Course: {student.course}");
+
+
+        MyModel mm = new MyModel();
+        mm.age = 62;
+        mm.name = "ExplicitOperator Method";
+
+        var myDto = (MyModelDTO)mm;
+        System.Console.WriteLine("My dto age using => " + myDto.age);
+
+    }
+
 }
 
-public class EmailService : IMessageService {
-    public void Send ( string message ) {
-        Console.WriteLine( $"Email sent: {message}" );
+
+
+// we can also utilize the implicit and explicit operators
+
+public class MyModel
+{
+    public int age;
+    public string? name;
+
+    // creating an explicit operator on user which will convert it into method name object type
+    public static explicit operator MyModelDTO(MyModel mm)
+    {
+        return new MyModelDTO
+        {
+            age = mm.age
+        };
     }
 }
 
-public class NotificationManager {
-    private readonly IMessageService _messageService;
-
-    public NotificationManager ( IMessageService messageService ) {
-        _messageService = messageService;
-    }
-
-    public void Notify ( string message ) {
-        _messageService.Send( message );
-    }
+public class MyModelDTO
+{
+    public int age;
 }
 
-class Program {
-    static void Main () {
-        var container = new SimpleContainer();
 
-        container.Register<IMessageService, EmailService>();
-        container.Register<NotificationManager, NotificationManager>();
 
-        var manager = container.Resolve<NotificationManager>();
-        manager.Notify( "Hello from DI container!" );
-    }
-}
+
+
